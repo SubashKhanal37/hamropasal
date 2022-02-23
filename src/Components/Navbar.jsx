6fcd451656e866b4210ef89932f1f2717f9e83b2
Bipul Khatiwada <khatiwadabipul@gmail.com>
2022-02-23 20:43:34 +0545

    navbar creation

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+   return (
+      <>
+      <nav className='flex bg-orange-400'>
+         <div>Hamro Pasal</div>
+      </nav>
+      </>
+   )
+}
+
+export default Navbar
